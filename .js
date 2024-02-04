ae1b4d5260b298bd8c1b86127cb019b07bc0726f
RohithaNaidu <rohitharakeshbabu@gmail.com>
2024-02-04 21:41:21 +0530

    after latte c

--- .js
+++ .js
@@ -1,12 +1,11 @@
 console.log("Hello, World!");
-<<<<<<< HEAD
+
 console.log("hi ")
 console,log("this is dhana");
 console.log("commpleted 2-3 commits in latte");
-=======
 console.log("hi");
 console.log("this is 2nd");
 console.log("hi");
-
+console.log("last")
 console.log("this is 2rd");
->>>>>>> latte
+
